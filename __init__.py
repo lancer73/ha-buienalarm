b56a0b1391a19c3d2552192c5f112ae7dc99b1b9
lancer73 <33584546+lancer73@users.noreply.github.com>
2025-06-12 17:46:34 +0200

    Update __init__.py
    Retry 2025.6 fix

--- __init__.py
+++ __init__.py
@@ -2,7 +2,14 @@
 De BuienAlarm integratie voor Home Assistant.
 """
 
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_NAME, Platform
+from homeassistant.core import HomeAssistant
+
 DOMAIN = "buienalarm"
+
+# List of platforms to set up
+PLATFORMS = [Platform.SENSOR]
 
 async def async_setup(hass, config):
     """Stel de BuienAlarm component in."""
@@ -11,10 +18,10 @@ async def async_setup(hass, config):
 async def async_setup_entry(hass, entry):
     """Stel een BuienAlarm config entry in."""
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     )
     return True
 
 async def async_unload_entry(hass, entry):
     """Verwijder een BuienAlarm config entry."""
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return await hass.config_entries.async_forward_entry_unload(entry, PLATFORMS)
